218bb388a30713f1f0cc1c64ac51e5561b4dc59b
Artem Grebenyk <a.hrebenyk@kitu.nau.edu.ua>
2025-10-31 22:49:46 +0200

    add lab9-10 constructors

--- lab910/DetailUserAccessTest.cpp
+++ lab910/DetailUserAccessTest.cpp
@@ -7,14 +7,14 @@ using namespace std;
 int main() {
     Detail detail(2, "Voltage: 1000V", 100);
 
-    User user;
+    User user("User");
     user.getInfo(detail);
 
-    Manager manager;
+    Manager manager("Manager");
     manager.setDescription(detail,"Voltage: 200V");
     manager.getInfo(detail);
 
-    Admin admin;
+    Admin admin("Admin");
     admin.setDescription(detail,"Voltage: 200V");
     admin.setPrice(detail, 340);
     admin.getInfo(detail);
--- lab910/Roles.cpp
+++ lab910/Roles.cpp
@@ -24,8 +24,25 @@ class EditPriceAccess {
 };
 
 class User : public ReadAccess {
+    std::string name;
+public:
+    User() : name("UnknownUser") {}
+    User(const string& name) : name(name) {}
+    User(const User& other) : name(other.name) {}
 };
-class Manager : public ReadAccess, public EditDescriptionAccess{
+
+class Manager : public ReadAccess, public EditDescriptionAccess {
+    string name;
+public:
+    Manager() : name("UnknownManager") {}
+    Manager(const string& name) : name(name) {}
+    Manager(const Manager& other) : name(other.name) {}
 };
+
 class Admin : public ReadAccess, public EditDescriptionAccess, public EditPriceAccess{
+    string name;
+public:
+    Admin() : name("UnknownAdmin") {}
+    Admin(const string& name) : name(name) {}
+    Admin(const Admin& other) : name(other.name) {}
 };
